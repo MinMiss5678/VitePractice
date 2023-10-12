ab16cbe2903b0386d60a9400b7078b864442a2a5
MinMiss5678 <83458914+MinMiss5678@users.noreply.github.com>
2023-10-12 16:07:18 +0800

    Vue pinia

--- vueapp/src/main.ts
+++ vueapp/src/main.ts
@@ -2,9 +2,16 @@ import './assets/main.css'
 import router from './router/index'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import type {PiniaPluginContext} from 'pinia'
 import App from './App.vue'
 
-const pinia = createPinia()
+export function myPiniaPlugin(context: PiniaPluginContext) {
+}
+const pinia = createPinia();
+pinia.use(myPiniaPlugin);
+pinia.use(({ store }) => {
+    store.hello = 'world'
+});
 const app = createApp(App);
 app.use(router);
 app.use(pinia);
